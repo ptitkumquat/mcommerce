7b97668cf67793baef4637ad03aeda09b414fce2
mlandouar <mlandouar@amiltone.com>
2019-09-09 15:05:44 +0200

    Creation de la page detail produit

--- clientui/src/main/java/com/clientui/proxies/MicroserviceProduitsProxy.java
+++ clientui/src/main/java/com/clientui/proxies/MicroserviceProduitsProxy.java
@@ -4,6 +4,7 @@ package com.clientui.proxies;
 import com.clientui.beans.ProductBean;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -12,6 +13,9 @@ public interface MicroserviceProduitsProxy {
 
     @GetMapping(value="/Produits")
     List<ProductBean>listeDesProduits();
+
+    @GetMapping(value="/Produits/{id}")
+    ProductBean recupererUnProduit(@PathVariable("id") int id);
 
 
 }
