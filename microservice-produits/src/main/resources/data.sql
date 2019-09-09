93d1416414f342575730de93f817891c72c5ce32
mlandouar <mlandouar@amiltone.com>
2019-09-09 14:35:18 +0200

    Creation clientui

--- microservice-produits/src/main/resources/data.sql
+++ microservice-produits/src/main/resources/data.sql
@@ -1,15 +1,15 @@
-INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (0, 'Bougie fonctionnant au feu', 'bougie qui fonctionne comme une ampoule mais sans électricité !', 'https://s3.eu-west-3.amazonaws.com/oc-images-cours/bougies.jpg', 22.0);
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (0, 'Super logo android', 'Juste un logo qui sert à rien', 'http://cdn2.blocksassets.com/assets/sct-home/blocks-responsive-site/pKHnh8VUhW4EqRq/web.BlocksGlobal-250x250px-banners-0000-Android-Intel.jpg', 22.0);
 
-INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (1, 'Chaise pour s''assoire', 'Chaise rare avec non pas 1 ni 2 mais 3 pieds', 'https://s3.eu-west-3.amazonaws.com/oc-images-cours/chaise.jpg', 95.0);
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (1, 'voyage voyage', 'Voyage en mongolfière au dessus de votre capitale préférée', 'http://www.drivebydigital.com/wp-content/uploads/2016/06/DbD-imagery-250x250px-15.png', 95.0);
 
-INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (2, 'Cheval pour nains', 'Ce cheval ne portera certainement pas blanche neige, mais sans problème les nains', 'https://s3.eu-west-3.amazonaws.com/oc-images-cours/cheval.jpg', 360.0);
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (2, 'Tisane miel lavande', 'Aide à combattre les maux de gorge et à dormir si vous la buvez avec un somnifère', 'https://srv3.parashop.com/29329-home_default/provence-d-antan-tisane-miel-lavande-bio-36g.jpg', 360.0);
 
-INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (3, 'Coq of steel, le superman des volailles', 'Ne passe pas au four', 'https://s3.eu-west-3.amazonaws.com/oc-images-cours/coq.jpg', 620.0);
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (3, 'Coq of steel, le superman des volailles', 'Ne passe pas au four', 'https://www.lesvergersdelagaline.com/wp-content/uploads/2019/01/250x250px-LL-077a8acc_faverolles-12-463279.jpeg', 620.0);
 
-INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (4, 'Flacon à frotter avec un génie dedans', 'Vous donne droit à l''équivalent de 3/0 voeux', 'https://s3.eu-west-3.amazonaws.com/oc-images-cours/flacon.jpg', 1200.0);
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (4, 'Un tas de vis', 'Après avoir monter 15 meubles IKEA, il restait tout ça...donc je les vends', 'https://vdm-prd01.acc.bricocdn.be/workshop/workshop/thumb/58/Fixations_murales_dans_divers_materiaux_250x250px.jpg', 1200.0);
 
-INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (5, 'Horloge quantique', 'Donne l''heure, les minutes et même les secondes. Ne fait pas de café', 'https://s3.eu-west-3.amazonaws.com/oc-images-cours/horloge.jpg', 180.0);
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (5, 'Lit très confortable', 'Pratique pour dormir, mais pas pour se réveiller', 'https://falschewimpern.org/wp-content/uploads/2018/02/housse-couette-bleu-canard-avec-housse-de-couette-bleu-canard-idees-et-housse-de-couette-en-lin-lave-bleu-canard-avec-550x550px.jpg', 180.0);
 
-INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (6, 'Table d''opération pour Hamsters', 'Pour réaliser vos opérations chirugicales sur votre Hamster!', 'https://s3.eu-west-3.amazonaws.com/oc-images-cours/table.jpg', 210.0);
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (6, 'Une scie éléctrique', 'Pour réaliser vos opérations chirugicales sur votre Hamster!', 'https://vdm-prd01.acc.bricocdn.be/workshop/workshop/thumb/36/Machines_____bois_250x250px.jpg', 210.0);
 
-INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (7 , 'Vase ayant appartenu a Zeus', 'Risque de choc électrique', 'https://s3.eu-west-3.amazonaws.com/oc-images-cours/vase.jpg', 730.0);
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (7 , 'Poulet fermier', 'Se mange chaud ou froid, mais sans les plumes de préférence', 'https://www.lesvergersdelagaline.com/wp-content/uploads/2017/08/pure-coucou.jpg', 730.0);
