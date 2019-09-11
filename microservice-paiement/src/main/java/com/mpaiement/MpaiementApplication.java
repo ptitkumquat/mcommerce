1c312afa1987814adbb880a2e70f7b53bd6f569d
mlandouar <mlandouar@amiltone.com>
2019-09-11 10:43:49 +0200

    Enregistrement des instances de tous les microservices dans eureka

--- microservice-paiement/src/main/java/com/mpaiement/MpaiementApplication.java
+++ microservice-paiement/src/main/java/com/mpaiement/MpaiementApplication.java
@@ -2,8 +2,12 @@ package com.mpaiement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@EnableDiscoveryClient
 public class MpaiementApplication {
 
 	public static void main(String[] args) {
