5911414813e95921bd7eb0a2acb845ee2864e055
mlandouar <mlandouar@amiltone.com>
2019-09-12 10:33:05 +0200

    création des premiers filtres zuul

---
+++ zuul-server/src/main/java/com/mcommerce/zuulserver/filters/ReponseFilter.java
@@ -0,0 +1,41 @@
+package com.mcommerce.zuulserver.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ReponseFilter extends ZuulFilter {
+
+    Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public String filterType() {
+        return "post";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return false;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+
+        HttpServletResponse response = RequestContext.getCurrentContext().getResponse();
+
+        response.setStatus(400);
+
+        log.info("Code HTTP que j'ai set moi-même et toc {}", response.getStatus());
+        return null;
+    }
+}
